51222a6e633ef4b8d39d3e2c482947581d08ba6e
moshiurrahmandeap11 <moshiurrahmandeap@gmail.com>
2026-05-10 20:24:04 +0600

    update axios instance

--- app/components/sharedComponents/AxiosInstance/AxiosInstance.jsx
+++ app/components/sharedComponents/AxiosInstance/AxiosInstance.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create instance
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8080/api", 
+  baseURL: "https://varsity-server.onrender.com/api", 
   withCredentials: true, 
 });
 
